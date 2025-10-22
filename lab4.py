3e7e4abda76828a6a5e3917c7ba5962c730482e9
ekinirmakbeyaz <ekinirmak.beyaz@std.yeditepe.edu.tr>
2025-10-23 02:00:34 +0300

    fixed

--- lab4.py
+++ lab4.py
@@ -1,13 +1,14 @@
 import numpy as np
+
 def throw_rock(m, v0, theta):
     g = 9.81  # in m/s^2
-    theta_rad = theta * np.pi / 180  # convert to radians for calculations
+    theta_rad = theta * np.pi / 180  # in radians
 
     tf = 2 * v0 * np.sin(theta_rad) / g  # in sec
     R = v0**2 * np.sin(2 * theta_rad) / g  # in m
     hm = v0**2 * np.sin(theta_rad)**2 / (2 * g)  # in m
     vh = v0 * np.cos(theta_rad)  # in m/s
-    Kh = 0.5 * m * vh**2 * hm  # in J
+    Kh = 0.5 * m * (vh * hm)**2  # in J
 
     print("For a rock with %5.3f kg mass thrown with %5.3f m/s at an angle of %6.2f degrees:\n"
           "Time of flight is %10.1e s\n"
@@ -18,6 +19,7 @@ def throw_rock(m, v0, theta):
           % (m, v0, theta, tf, R, hm, vh, Kh))
 
     return tf, R, hm, vh, Kh
+
 
        
 
